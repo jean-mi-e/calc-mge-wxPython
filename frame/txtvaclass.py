0ecf02d4274c09fd1233300eedef6b4d3c8629f8
jean-mi-e <jmezrati@yahoo.fr>
2020-08-25 17:07:11 +0200

    Update txtvaclass.py

--- frame/txtvaclass.py
+++ frame/txtvaclass.py
@@ -3,28 +3,32 @@
 import wx
 
 
-class Classtxtva(wx.Frame):
+class Classtxtva(wx.Panel):
 	"""Classe du tk.Entry Taux de la TVA"""
 
-	def __init__(self, fenetre):
-		super(Classtxtva, self).__init__()
+	def __init__(self, parent):
+		wx.Panel.__init__(self, parent)
 
-		self.lab_txtva = wx.StaticText(fenetre, -1, "Taux de T.V.A.", (210, 5))
+		self.lab_txtva = wx.StaticText(self, -1, "Taux de T.V.A.")
 		self.listTaux = ["20 %", "10 %", "5,50 %", "2,10 %", "0 %", "Inconnu"]
-
-		self.combo = wx.ComboBox(fenetre, -1, value=self.listTaux[0], choices=self.listTaux, pos=(210, 25),
+		self.txtva = wx.ComboBox(self, -1, value=self.listTaux[0], choices=self.listTaux,
 								 style=wx.CB_READONLY | wx.CB_DROPDOWN)
 
-		sizer = wx.GridBagSizer()
-		sizer.Add(self.combo, (210, 25), (1, 1), wx.EXPAND)
+		sizer = wx.BoxSizer(wx.VERTICAL)
+		sizer.Add(self.lab_txtva)
+		sizer.Add(self.txtva, wx.ALIGN_LEFT)
+		sizer.Add(-1, 10)
+		self.SetSizer(sizer)
+		#sizer = wx.GridBagSizer()
+		#sizer.Add(self.combo, (210, 25), (1, 1), wx.EXPAND)
 
 	def val_get(self):
 		"""Méthode permettant de vérifier si l'entrée est numérique et de retourner un float"""
 
-		if self.combo.GetStringSelection() == self.listTaux[5]:
+		if self.txtva.GetStringSelection() == self.listTaux[5]:
 			txselec = self.listTaux[5]
 		else:
-			txselec = self.combo.GetStringSelection()
+			txselec = self.txtva.GetStringSelection()
 			txselec = txselec[:-2]
 			if txselec == '5,50':
 				txselec = 5.5
@@ -42,7 +46,7 @@ class Classtxtva(wx.Frame):
 	def delete(self):
 		"""Méthode effaçant le contenu de l'Entry de la classe"""
 
-		return self.combo.SetSelection(self.listTaux[0])
+		return self.txtva.SetSelection(self.listTaux[0])
 
 	def insert(self, arg):
 		"""Méthode permettant de définir le taux de TVA dans l'OptionMenu"""
@@ -50,14 +54,14 @@ class Classtxtva(wx.Frame):
 		val_arg = {float(20): '0', float(10): '1', 5.5: '2', 2.1: '3', float(0): '4', "Inconnu": 5}
 		if arg in val_arg.keys():
 			arg = val_arg[arg]
-			return self.combo.SetValue(self.listTaux[int(arg)])
+			return self.txtva.SetValue(self.listTaux[int(arg)])
 		else:
 			res = wx.MessageDialog(self, "Vous avez du faire une erreur de saisie \n"
 										 f"Le Taux de TVA {arg:.2f}% n'existe pas.", "ERREUR",
 								   style=wx.OK | wx.ICON_ERROR | wx.CENTRE, pos=wx.DefaultPosition)
 			res = res.ShowModal()
-			return self.combo.SetValue(arg)
+			return self.txtva.SetValue(arg)
 
 	def raz(self):
 
-		self.combo.SetValue(self.listTaux[0])
+		self.txtva.SetValue(self.listTaux[0])
